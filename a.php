c0f084da3fa0459de8ea2320d468e724adc460b8
Valdas <valdas@e-servisas.lt>
2017-11-14 18:28:12 +0200

    Naujas failas a

--- a.php
+++ a.php
@@ -1,7 +1,7 @@
 <!Doctype html>
 <html>
 <head>
-<title>Užduotis Nr.1 ir Nr.2</title>
+<title>Branch master: Užduotis Nr.1 ir Nr.2</title>
 <style>
     .pirmas { background-color: #0b530e;
               color: #ffffff; } 
